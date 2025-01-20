b01241697e0bc48050d6eabda6208cbf9fffc8f3
Arghyadeep Karmakar <arghyadeep.karmakar@publicissapient.com>
2025-01-20 07:31:59 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,56 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+
+import NextBundleAnalyzer from "@next/bundle-analyzer";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    },
+    typescript: {
+        tsconfigPath: "tsconfig.json",
+    },
+    bundlePagesRouterDependencies: true,
+    reactStrictMode: false,
+    experimental: {
+        optimizePackageImports: ["lucide-react"],
+        serverActions: {
+            bodySizeLimit: "5mb",
+        },
+        staleTimes: {
+            dynamic: 30,
+            static: 180,
+        },
+        turbo: {
+            rules: {
+                "*.svg": {
+                    loaders: ["@svgr/webpack"],
+                    as: "*.js",
+                },
+            },
+            resolveAlias: {
+                underscore: "lodash",
+                mocha: { browser: "mocha/browser-entry.js" },
+            },
+            resolveExtensions: [
+                ".mdx",
+                ".tsx",
+                ".ts",
+                ".jsx",
+                ".js",
+                ".mjs",
+                ".json",
+            ],
+        },
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+        dirs: ["src"],
+    },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = NextBundleAnalyzer({
+    enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
